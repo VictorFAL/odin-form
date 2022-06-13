d8988f523b43293d830923e3937b1cf03f764c18
Victor | Harry <victorfalmeida12@gmail.com>
2022-06-13 15:04:42 -0300

    form layout + img credit

---
+++ script.js
@@ -0,0 +1,7 @@
+btnSubmit = document.querySelector('button');
+inputPass = document.getElementById('pw');
+inputConfPass = document.getElementById('cpw')
+
+btnSubmit.addEventListener(() => {
+    
+});
